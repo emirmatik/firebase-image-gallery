546023b59dc4a79d2cac1e42a3ebc69854409fab
Emir <emirmatik16@gmail.com>
2020-08-06 21:24:36 +0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,13 +2,7 @@ import firebase from "firebase";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyDYk6xjOZfiuVIx-B77t77r54Id8ikRZ7o",
-  authDomain: "image-gallery-1a013.firebaseapp.com",
-  databaseURL: "https://image-gallery-1a013.firebaseio.com",
-  projectId: "image-gallery-1a013",
-  storageBucket: "image-gallery-1a013.appspot.com",
-  messagingSenderId: "961106933351",
-  appId: "1:961106933351:web:90cdbf7ad124dd7d3386c2",
+  // PASTE_HERE_YOUR_CONFIG_OPTIONS
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
